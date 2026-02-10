893930047dbdd62ad0c586021d5ddc80d8acc44c
Jinwoo <wjdwlsdn8520@gmail.com>
2026-02-10 21:30:38 +0900

    test11

--- test_code.py
+++ test_code.py
@@ -1,12 +1,12 @@
-import numpy as np
 import cv2
 import logging
+import math
 
 for i in range(0,2):
     i += 1
     print(f"i: {i}")
 
-logging.error("test code")
+logging.error("test code11111111")
 logging.info("test code")
 
 print("just test, take it easy")
