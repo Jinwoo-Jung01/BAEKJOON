be2a6f07bc7fc53e424cc658ea444b9e41dd750d
Jinwoo <wjdwlsdn8520@gmail.com>
2026-02-10 21:09:29 +0900

    test

---
+++ test_code.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+import logging
+
+for i in range(0,5):
+    i += 1
+    print(f"i: {i}")
+
+logging.error("test code")
+logging.info("test code")
+
+print("just test, take it easy")
