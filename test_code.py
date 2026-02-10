d4f429b9356ee4b2b29befcce8dfd0c0404e26b9
Jinwoo <wjdwlsdn8520@gmail.com>
2026-02-10 21:51:29 +0900

    Before merge

--- test_code.py
+++ test_code.py
@@ -3,7 +3,7 @@ import numpy as np
 import logging
 import math
 
-for i in range(0,2):
+for i in range(0,10):
     i += 1
     print(f"i: {i}")
 
@@ -11,4 +11,16 @@ logging.error("test code2222222")
 logging.info("test code")
 
 print("just test, take it easy")
+<<<<<<< HEAD
 print("hi this is test 2")
+=======
+print("just test, take it easy")
+print("just test, take it easy")
+print("just test, take it easy")
+print("just test, take it easy")
+print("just test, take it easy")
+print("just test, take it easy")
+print("just test, take it easy")
+print("just test, take it easy")
+print("just test, take it easy")
+>>>>>>> 7fafd87 (Before merge)
